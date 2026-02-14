b12b769c23ab1285d61f9d08ba96c92833b36ddb
otaiwo <taiwosolodun@gmail.com>
2026-02-14 06:57:00 +0100

    describe what you changed

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'Assetly API is alive 🔥'
+    ]);
+});
